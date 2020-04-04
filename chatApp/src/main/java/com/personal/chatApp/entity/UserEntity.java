b9c0a532cb919d0e842ec785817925373ad2ab98
SIDDHANT JAIN <sidjainsid05@gmail.com>
2020-04-04 21:15:45 +0530

    added user entity and user model

--- chatApp/src/main/java/com/personal/chatApp/entity/UserEntity.java
+++ chatApp/src/main/java/com/personal/chatApp/entity/UserEntity.java
@@ -1,5 +1,48 @@
 package com.personal.chatApp.entity;
 
-public class UserEntity {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="USERS")
+public class UserEntity {
+	
+	@Id
+	private String username;
+	private String emailId;
+	private String name;
+	private String phoneNumber;
+	private String password;
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
--- chatApp/src/main/java/com/personal/chatApp/model/User.java
+++ chatApp/src/main/java/com/personal/chatApp/model/User.java
@@ -1,5 +1,39 @@
 package com.personal.chatApp.model;
 
 public class User {
-
+	private String emailId;
+	private String username;
+	private String name;
+	private String phoneNumber;
+	private String password;
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
